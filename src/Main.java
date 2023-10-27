22294aea8a7833bdb998bd27c0ae12e78ed49cfb
Rasmus Torell <rasmustorell@AirsomtorRasmus.lan>
2023-10-27 15:06:10 +0200

    Cleaned up master branch

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,6 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-       // new GameLayout();
-        TileGenerator tg = new TileGenerator();
-
-
-        List<String> testList = new ArrayList<>();
-        testList.add("1"); testList.add("2"); testList.add("3");testList.add("4");
-        Collections.shuffle(testList);
-
-        List<Tiles> lista = tg.createListOfTiles(testList, 2,2);
-
-        for (Tiles tile: lista){
-            System.out.println("name " + tile.getText());
-            System.out.println(tile.getRowNr());
-            System.out.println(tile.getColumnNr());
-            System.out.println("---");
-        }
-
-
-
-
 
         //New branch added - Development
     }
