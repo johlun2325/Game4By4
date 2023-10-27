237bbaa200ec6079ff2b840127dbd518b4f0223c
Johanna Lundström <johanna.ml.lundstrom@gmail.com>
2023-10-27 10:33:19 +0200

    Added class ButtonAction, and change to new GameLayout(); in main.

---
+++ src/ButtonAction.java
@@ -0,0 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonAction implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("klick");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        GameLayout g = new GameLayout();
+        new GameLayout();
     }
 }
